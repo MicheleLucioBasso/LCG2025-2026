45e5c221f8bd072937e4f308cf1d5fc14f2264fe
MicheleLucioBasso <michelelucio.basso@mail.polimi.it>
2025-10-07 12:30:11 +0200

    MicheleLucioBasso

--- esercitazione 30 settembre/sketch.js
+++ esercitazione 30 settembre/sketch.js
@@ -8,13 +8,27 @@ let table;
 let star_img;
 
 function preload() { //carica le risorse, prima del setup, es. dati, immagini ecc.
-  table = loadTable("stars.csv", "csv", "header");
-  star_img = loadImage("star.png");
+  table = loadTable("stars.csv", "csv", "header"); //tabella dati csv
+  star_img = loadImage("star.png"); //immagine stella
 }
 
 function setup() {
   createCanvas(xMax, yMax);
-  frameRate(5);
+  frameRate(10);
+}
+
+function drawSingleStarFromFile(index, posX, posY) { //vogliamo tracciare tante stelle quante sono le righe della tabella, ogni stella deve avere grandeza corrispondete a numero nella riga
+  let starSize = table.getNum(index, "starSize") //usiamo nome starSize, poiché è quello che sta scritto nell'header della tabella
+  image(star_img, posX, posY, starSize, starSize);
+}
+
+function drawStarsFromFile () { //questo ciclo dice vai avanti fintanto che k (il numero della riga corrente) è minore del numero di righe totali della tabella
+  for (let k = 0; k < table.getRowCount(); k++) {//getRowCount restituisce il numero di righe della tabella, escluso l'header
+    let starX = (k*37) % width + (k%3) * 5;
+    let starY = (k*73) % height + (k%7);
+
+    drawSingleStarFromFile(k, starX, starY);
+  }
 }
 
 function drawSingleStar(i, starX, starY, random_transparency, random_size) {
@@ -47,6 +61,10 @@ function drawStars(num_stars=120) {
 
     drawSingleStar(i, starX, starY, random_transparency, random_size);
   }
+}
+
+function drawRocketFromFile(xRocket, yRocket) {
+  image(rocket_img, xRocket, yRocket);
 }
 
 function drawRocket (xRocket, yRocket) {
@@ -93,9 +111,13 @@ function draw() {
   //1 unico ciclo che crea una sequenza a, b, c
   noStroke();
 
-  drawStars(100); //in base a che parametro scrivo come argomento, avrò quel numero di stelle es. 100
+  //drawStars(100); //in base a che parametro scrivo come argomento, avrò quel numero di stelle es. 100
 
-  drawRocket(xRocket, yRocket);
+  drawStarsFromFile();
+
+  //drawRocket(xRocket, yRocket);
+
+  drawRocketFromFile(xRocket, yRocket);
 
   yRocket = moveRocket(yRocket, 1);
 
