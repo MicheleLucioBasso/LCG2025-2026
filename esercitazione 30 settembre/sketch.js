9086e50b7a781c9caca247b2638b799179425322
MicheleLucioBasso <michelelucio.basso@mail.polimi.it>
2025-10-07 12:06:54 +0200

    MicheleLucioBasso

--- esercitazione 30 settembre/sketch.js
+++ esercitazione 30 settembre/sketch.js
@@ -4,9 +4,80 @@ let yMax = 600;
 let xRocket = xMax/2; //così dico già che il razzo partirà da metà del foglio
 let yRocket = yMax*0.6;
 
+let table;
+let star_img;
+
+function preload() { //carica le risorse, prima del setup, es. dati, immagini ecc.
+  table = loadTable("stars.csv", "csv", "header");
+  star_img = loadImage("star.png");
+}
+
 function setup() {
   createCanvas(xMax, yMax);
   frameRate(5);
+}
+
+function drawSingleStar(i, starX, starY, random_transparency, random_size) {
+  //operatore modulo %
+    //stella a quando i è pari
+    if( i % 2 == 0){
+      //stella a
+    fill(255,255,150);
+    ellipse(starX, starY, random_size);
+    } else if ( i % 3 == 0){
+      //stella b
+      //ci saranno per ogni i che è divisibile per 3
+      fill(200, 100, 255, random_transparency);
+      ellipse(starX, starY, random_size);
+    }else{
+      //stella c
+      fill(255, 255, 100);
+      ellipse(starX, starY, random_size);
+    }
+    return;
+}
+
+function drawStars(num_stars=120) {
+  for(let i=0; i < num_stars; i++) { //i=0 è l'inizializzazione, quindi parte da 0, i<40 fa arrivare variabile fino a 39 poi riparte, i++ aggiugne ogni volta 1 stella
+    let starX = (i*37) % width + (i%3) * 5;
+    let starY = (i*73) % height + (i%7);
+
+    let random_transparency = random(150, 255);
+    let random_size = random(6.8, 15.0);
+
+    drawSingleStar(i, starX, starY, random_transparency, random_size);
+  }
+}
+
+function drawRocket (xRocket, yRocket) {
+  push(); //inizia contesto di disegno
+  //rettangolo
+  fill(220);
+  stroke(40);
+  rectMode(CENTER); //alternativa di disegno, mi fa basare coordinate rettangolo dal centro
+  rect(xRocket,yRocket+30,80,180,20); //ultimo valore smussa gli angoli del rettangolo
+  
+  //triangolo
+  fill(200,40,40);
+  triangle(xRocket-40, yRocket-60, xRocket+40, yRocket-60, xRocket, yRocket-120);
+  
+  //cerchio
+  fill(40,150,220);
+  stroke(255);
+  strokeWeight(3);
+  ellipse(xRocket,yRocket+30,48,48);
+
+  //finire contesto di disegno
+  pop();
+}
+
+function moveRocket(yRocket, step=1) {
+  yRocket = yRocket - step;
+  let soglia = -(yMax * 0.6);
+  if (yRocket < soglia) {
+    yRocket = yMax;
+  }
+  return yRocket;
 }
 
 function draw() {
@@ -22,30 +93,16 @@ function draw() {
   //1 unico ciclo che crea una sequenza a, b, c
   noStroke();
 
-  for(let i=0; i < 120; i++){ //i=0 è l'inizializzazione, quindi parte da 0, i<40 fa arrivare variabile fino a 39 poi riparte, i++ aggiugne ogni volta 1 stella
-    let starX = (i*37) % width + (i%3) * 5; //width è una variabile primitiva di p5 che prende in automatico l'ampiezza del foglio
-    let starY = ((i*73) % height) + (1%7);
-    //operatore modulo %
-    //stella a quando i è pari
-    if( i % 2 == 0){
-      //stella a
-    fill(255,255,150);
-    ellipse(starX,starY,1);
-    }else if ( i % 3 == 0){
-      //stella b
-      //ci saranno per ogni i che è divisibile per 3
-      fill(200, 100, 255);
-      ellipse(starX, starY, 1.5);
-    }else{
-      //stella c
-      fill(255, 255, 100);
-      ellipse(starX, starY, 2.8);
-    }
-  }
-  pop();
+  drawStars(100); //in base a che parametro scrivo come argomento, avrò quel numero di stelle es. 100
 
+  drawRocket(xRocket, yRocket);
+
+  yRocket = moveRocket(yRocket, 1);
+
+  pop();
+  /*
     //contesto di disegno serve a isolare pezzo di disegno tra una push e una pop per non far influenzare gli attributi che diamo a un certo disegno anche agli altri
-  push();
+  push(); //in questo contesto di disegno ci sono le righe id codice che disegnano il razzo
 
   //rettangolo
   fill(220);
@@ -65,11 +122,14 @@ function draw() {
 
   //finire contesto di disegno
   pop();
-
+  */
+  
+  /*
   xRocket = (xRocket +1) % xMax;
   yRocket = (yRocket -1); //quando la yRocket sarà oltre una certa soglia, allora dobbiamo resettare la yRocket
   let soglia = yMax*0.6;
   if(yRocket < soglia){
     yRocket = yMax;
   }
+  */
 }
